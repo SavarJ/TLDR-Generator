eb1f6d2745a8df743897592aecbf6b114162f31c
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2022-05-18 19:12:22 -0400

    Delete boilerplate and load up tailwindcss

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-red-500 underline">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
