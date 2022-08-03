72baefd5155c3a0e8851c802edd247cfc19b5e5b
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-08-03 17:41:43 -0400

    Add better error handling

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import AnswerCard from "./AnswerCard";
 import Button from "./Button";
 import Footer from "./Footer";
 import Heading from "./Heading";
@@ -8,41 +7,65 @@ import TextArea from "./TextArea";
 import { Answer } from "./Answer";
 import Results from "./Results";
 
-const BACKEND_API_URL = "http://localhost:5050/api/answers/";
+const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 const App: React.FC = () => {
   const [textarea, setTextarea] = useState("");
   const [answers, setAnswers] = useState<Answer[]>([]);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch(BACKEND_API_URL);
-      const data = await response.json();
-      setAnswers(data.data);
+      if (!BACKEND_API_URL) {
+        alert(
+          `Unable to load the backend api url. Currently the value is ${BACKEND_API_URL}`
+        );
+        setButtonDisabled(true);
+        return;
+      } else {
+        const response = await fetch(BACKEND_API_URL);
+        const data = await response.json();
+        setAnswers(data.data);
+      }
     };
     getData();
   }, []);
 
   const handleClick = async () => {
     try {
-      const response = await fetch(BACKEND_API_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: `${textarea.slice(0, 500)}`,
-        }),
-      });
+      if (!BACKEND_API_URL) {
+        alert(
+          `Unable to load the backend api url. Currently the value is ${BACKEND_API_URL}`
+        );
+        setButtonDisabled(true);
+        return;
+      } else {
+        const response = await fetch(BACKEND_API_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: `${textarea.slice(0, 500)}`,
+          }),
+        });
 
-      const answers = await response.json();
-      setAnswers(answers.data);
-      setTextarea("");
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const answers = await response.json();
+        if (answers.statusCode === 500) {
+          throw new Error();
+        }
+        setAnswers(answers.data);
+        setTextarea("");
+      }
     } catch (e) {
       console.error(e);
       alert("An error occurred, please try again later");
       alert(
-        "If you try running this app but get an error, it is likely due to OPEN AI recycling the API KEY."
+        "If you try running this app but get an error, it is likely due my free tier being exhausted. Please try again later."
       );
     }
   };
@@ -50,7 +73,11 @@ const App: React.FC = () => {
     <MainWrapper>
       <Heading />
       <TextArea textarea={textarea} setTextarea={setTextarea} />
-      <Button text="Generate!" handleClick={handleClick} />
+      <Button
+        text="Generate!"
+        handleClick={handleClick}
+        disabled={buttonDisabled}
+      />
       <hr />
       <Results array={answers} />
       <Footer />
