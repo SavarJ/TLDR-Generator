7a13eb288c0ab0e55aec6ea8921fc6282906b61a
Savar Jain - Home MBA <jainsavar@gmail.com>
2022-08-03 17:03:34 -0400

    Add prompt and answer to answer card

--- frontend/src/components/AnswerCard.tsx
+++ frontend/src/components/AnswerCard.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+
 interface AnswerCardProps {
+  prompt: string;
   answer: string;
 }
-const AnswerCard: React.FC<AnswerCardProps> = ({ answer }) => {
+const AnswerCard: React.FC<AnswerCardProps> = ({ prompt, answer }) => {
+  const [fullyVisisble, setFullyVisisble] = useState(false);
   return (
-    <div className="border-2 p-4 border-white rounded-md shadow-lg hover:shadow-xl shadow-white hover:shadow-white">
-      <h2 className="text-center font-bold text-2xl tracking-tight">Answer</h2>
-      <p className="text-center font-mono text-xl tracking-tight">{answer}</p>
+    <div
+      className={`border-2 p-4 max-w-2xl border-white rounded-md shadow-lg hover:shadow-xl shadow-white hover:shadow-white cursor-pointer transition-all duration-500 ease-in-out
+      ${!fullyVisisble ? "max-h-80 overflow-y-hidden" : "h-auto"}
+      `}
+      onClick={() => setFullyVisisble(!fullyVisisble)}
+    >
+      <h2 className="font-bold text-lg tracking-tight">
+        TL;DR -{">"} {answer}
+      </h2>
+      <br />
+      <div
+        className={`text-center font-mono text-base tracking-tight max-w-2xl
+        ${
+          !fullyVisisble
+            ? "text-ellipsis whitespace-nowrap overflow-hidden"
+            : ""
+        }
+        `}
+      >
+        Original: {prompt}
+      </div>
     </div>
   );
 };
